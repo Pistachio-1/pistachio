b46c73a8111d22b7710463ce8943dcbbafabf8ff
Michelle Shin <mshin017@ucr.edu>
2018-05-30 11:36:27 -0700

    updating card slides

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -81,6 +81,21 @@ $(".dayOneButton").click(function() {
     });
 });
 
+(function() {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ 
+        $('.carousel').carousel('next');
+        return false; 
+    });
+    $('.prev').click(function(){ 
+        $('.carousel').carousel('prev');
+        return false; 
+    });
+    
+})
+
 
 
 
